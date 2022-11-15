6f2eec67d445f264fd74ca78705fa956ce7dc8b0
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-15 17:16:16 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -12,7 +12,6 @@ from config import (
     BOT_USERNAME,
     GROUP_SUPPORT,
     OWNER_NAME,
-    SUDO_USERS,
     BOT_TOKEN,
     UPDATES_CHANNEL,
 )
@@ -56,7 +55,7 @@ async def _human_time_duration(seconds):
     return ", ".join(parts)
 
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(command("/start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{BOT_PHOTO}",
