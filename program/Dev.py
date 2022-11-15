b6438f62f704f19e42629a2c4e1a947f25f63f7a
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-15 18:20:14 +0200

    Update Dev.py

--- program/Dev.py
+++ program/Dev.py
@@ -14,7 +14,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, 
 @Client.on_message(command(["مطور البوت", "المطور", "مطور"]))
 async def muamen(client: Client, message: Message):
   usrr = await client.get_users(SUDO_USERS)
-  userr = await client.get_chat(SUDO_USERS)
+  userr = await client.get_chat("1970797144")
   shadow = usrr.first_name
   namee = usrr.mention
   uuser = usrr.username
