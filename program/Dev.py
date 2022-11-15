bf5d352ee94b0e9dd39feffc58b01dc291a00c5b
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-15 17:02:44 +0200

    Update Dev.py

--- program/Dev.py
+++ program/Dev.py
@@ -16,11 +16,12 @@ async def muamen(client: Client, message: Message):
   usr = await client.get_users("1970797144")
   user = await client.get_chat("1970797144")
   shad = usr.first_name
+  mua = usr.mention
   useer = usr.username
   Bio = user.bio
   await message.reply_photo(
     photo=f"https://t.me/S550D",
-    caption=f"""**▷ ᴅᴇᴠ sᴏᴜʀᴄᴇ ʟụɴᴀ ѕʜᴀᴅᴏᴡ ♯**\n**▷ɴᴀᴍᴇ ѕʜᴀᴅᴏᴡ ⇿** {shad}\n**▷ ɪᴅ ѕʜᴀᴅᴏᴡ ⇿** 1970797144\n**▷ ʙɪᴏ ѕʜᴀᴅᴏᴡ ⇿** {Bio}""",
+    caption=f"""**▷ ᴅᴇᴠ sᴏᴜʀᴄᴇ ʟụɴᴀ ѕʜᴀᴅᴏᴡ ♯**\n**▷ɴᴀᴍᴇ ѕʜᴀᴅᴏᴡ ⇿** {mua}\n**▷ ɪᴅ ѕʜᴀᴅᴏᴡ ⇿** 1970797144\n**▷ ʙɪᴏ ѕʜᴀᴅᴏᴡ ⇿** {Bio}""",
     reply_markup=InlineKeyboardMarkup(
         [
             [
@@ -30,6 +31,34 @@ async def muamen(client: Client, message: Message):
             [
                 InlineKeyboardButton(
                     shad, url=f"https://t.me/{useer}"
+            ),
+            ],
+            [
+                InlineKeyboardButton(
+                   "ضيـف البـوت لمجمـوعتـك ✅", url=f"https://t.me/S88DBOT?startgroup=true"
+                ),
+            ],
+        ]
+      )
+    )
+
+
+@Client.on_message(command(["مطور البوت", "المطور", "مطور"]))
+async def muamen(client: Client, message: Message):
+  usr = await client.get_users(SUDO_USERS)
+  user = await client.get_chat(SUDO_USERS)
+  shadow = usr.first_name
+  namee = usr.mention
+  uuser = usr.username
+  Bioo = user.bio
+  await message.reply_photo(
+    photo=f"https://t.me/S550D",
+    caption=f"""❲ **Developer Bot** ❳\n— — — — — — — — —\n𖥔 **Dev Name :** {namee}\n𖥔 **Dev User :** @{uuser}\n𖥔 **Dev Id :** {SUDO_USERS}\n𖥔 **Dev Bio :**{Bioo}""",
+    reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    shadow, url=f"https://t.me/{uuser}"
             ),
             ],
             [
