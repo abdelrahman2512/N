34caeecee362815aadfc4ddcdcd7b5291ace0c50
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-15 16:51:14 +0200

    Update Dev.py

--- program/Dev.py
+++ program/Dev.py
@@ -3,7 +3,7 @@ import asyncio
 from sys import version_info
 from program import __version__
 from driver.veez import user
-from config import OWNER_ID
+from config import SUDO_USERS
 from driver.filters import command, other_filters
 from pyrogram import Client, filters, __version__ as pyrover
 from pyrogram.errors import FloodWait, MessageNotModified
