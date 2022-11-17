925ff2debb17aa0a11087615e0b0897306811fef
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-17 09:29:47 +0200

    Update updater.py

--- program/updater.py
+++ program/updater.py
@@ -54,7 +54,7 @@ def updater():
     return bool(changelog)
 
 
-@Client.on_message(command(["update", f"update@{BOT_USERNAME}"]) & ~filters.edited)
+@Client.on_message(command(["update", "تحديث السورس"]) & ~filters.edited)
 @sudo_users_only
 async def update_repo(_, message: Message):
     chat_id = message.chat.id
