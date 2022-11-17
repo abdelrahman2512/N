49c782013ba9238aaddd7b83e17a189e29f3fe1d
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-17 22:13:12 +0200

    Update aghany.py

--- program/aghany.py
+++ program/aghany.py
@@ -8,3 +8,23 @@ from pyrogram import Client, filters, __version__ as pyrover
 from pyrogram.errors import FloodWait, MessageNotModified
 from pytgcalls import (__version__ as pytover)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
+
+
+@Client.on_message(command(["اغاني"]) & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""🦅 اهلا بيك بقائمه تصنيفات الاغاني اختر ما تريد\n√
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("اغاني عربي 🇪🇬", callback_data="arb")],
+                [InlineKeyboardButton("اغاني اجنبي 🇦🇺", callback_data="eng")],
+                [
+                    InlineKeyboardButton(
+                        "ضيـف البـوت لمجمـوعتـك ✅", url=f"https://t.me/{BOT_USERNAME}?startgroup=new"
+                    )
+                ],
+            ]
+        ),
+    )
