ffaee8f6daef01d09f7621ff521967a726d65ea0
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-17 19:38:06 +0200

    Update Infouser.py

--- program/Infouser.py
+++ program/Infouser.py
@@ -8,24 +8,3 @@ from pyrogram import Client, filters, __version__ as pyrover
 from pyrogram.errors import FloodWait, MessageNotModified
 from pytgcalls import (__version__ as pytover)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
-
-
-@aClient.on_message(
-    command(["اسمي"])
-    & ~filters.edited
-)
-async def khalid(client: Client, message: Message):
-    usr = await client.get_users(message.from_user.id)
-    name = usr.first_name
-    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
-                    await message.reply_text( 
-                    f"""ٲسـمـك💕 ⇐ {message.from_user.mention}""", 
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/Source_Frozen"),
-                ],
-            ]
-        ),
-    )
